d7bdd3e2ca30b2ec571b401fc87cddbd5f639ae6
Mr Mendes <mendes.paul28@gmail.com>
2022-03-19 02:29:36 +0100

    circleci testing 12345

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 12
+          Learn React 12345
         </a>
       </header>
     </div>
